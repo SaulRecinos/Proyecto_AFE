7cbd96817dbdeb5e2f771414400753958b4341af
SaulRecinos <saul.batres20@itca.edu.sv>
2026-05-20 00:01:33 -0600

    fix: change in and resolve conflict ci.yml

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -12,8 +12,12 @@ class ExampleTest extends TestCase
      */
     public function test_the_application_returns_a_successful_response(): void
     {
-        $response = $this->get('/');
-
+        $this->seed(\Database\Seeders\AdminModuleSeeder::class);
+    
+        $user = \App\Models\User::where('email', 'admin@proyectoafe.com')->first();
+    
+        $response = $this->actingAs($user)->get('/');
+    
         $response->assertStatus(200);
     }
 }
